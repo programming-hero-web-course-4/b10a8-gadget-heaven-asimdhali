64a4d0683697d981d7795dff2a150e30b8b967ad
1asimdhali <asim.dhali.bd@gmail.com>
2024-11-15 19:01:40 +0600

    navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './components/Root/Root.jsx';
+import Home from './components/Home/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <div>url sothik kore lekh mama 404</div>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+
+      },
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
