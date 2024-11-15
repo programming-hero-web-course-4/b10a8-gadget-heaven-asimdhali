194de6694223b78ea9e4709d59b198ed985892d7
1asimdhali <asim.dhali.bd@gmail.com>
2024-11-15 19:34:57 +0600

    navbar and footer section update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,10 @@
+import { Link } from "react-router-dom";
+import { FaCartArrowDown } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 container mx-auto">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -24,7 +28,7 @@ const Navbar = () => {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
             <li>
-              <a>Item 1</a>
+              <a>Home</a>
             </li>
             <li>
               <a>Parent</a>
@@ -42,33 +46,35 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <Link to="/" className="btn btn-ghost text-xl">Gadget Heaven</Link>
       </div>
+
+
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Item 1</a>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
+            <Link to="/statistics">Statistics</Link>
           </li>
           <li>
-            <a>Item 3</a>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
           </li>
         </ul>
       </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
+
+
+      <div className="navbar-end flex gap-4">
+        <Link>
+            <div><FaCartArrowDown /></div>
+        </Link>
+        <Link>
+            <div><FaHeart /></div>
+        </Link>
       </div>
     </div>
   );
